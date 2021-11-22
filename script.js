beb3f8e6f4fb3e95e538cc2e627e0041b3d5981d
Jeremiah <dayoolapeju@outlok.com>
2021-11-22 10:48:40 +0100

    version 1

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 const container = document.querySelector('.container');
-
-let cols = 16,
-    rows = 16;
+const newGridBtn = document.querySelector('.new-grid');
 
 // print out a 16 x 16 grid
-let y = 0;
-while(y < rows) {
-    let x = 0;
-    while(x < cols){
+function printGrid(num) {
+    // clear container
+    container.innerHTML = '';
+
+    // set container grid-template-columns property
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    // container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
+    let y = 0;
+    while(y < num * num) {
         // create divs
         let div = document.createElement('div');
-        div.textContent = ' ';
-        // insert it into container div
+        div.classList.add('grid-item');
+        div.addEventListener('mouseover', paint);
         container.appendChild(div);
-        x++;
+        y++;
     }
-    y++;
+
+    document.querySelectorAll('.container div').forEach(el => el.style.height = el.offsetWidth);
 }
+
+function createNewGrid(e) {
+    let dimension = prompt("Please enter the number of squares per side for the new grid");
+    dimension = Number(dimension);
+    
+    if(!dimension || dimension == NaN){
+        return;
+    }
+
+    if(dimension >= 100) {
+        alert("Sorry! you can't go beyond 99 squares.");
+        return;
+    }
+
+    // print new grid
+    printGrid(dimension);
+}
+
+function paint(e) {
+    e.target.style.backgroundColor = "#000";
+}
+
+// Display grid
+printGrid(16);
+
+newGridBtn.addEventListener('click', createNewGrid);
 
